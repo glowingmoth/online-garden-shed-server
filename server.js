c7a2fe12eaeb95022a1877a6a9a465f013e25df3
jasonkim7288 <jason.kim7288@gmail.com>
2020-12-12 14:52:57 +1000

    add cors

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 const passport = require('passport');
@@ -29,6 +30,19 @@ mongoose.connect(
 const app = express();
 
 app.use(logger('dev'));
+
+const whiteList = ['http://localhost:3000', 'https://gardenshed.netlify.app'];
+app.use(cors({
+  credentials: true,
+  origin: (origin, callback) => {
+    if (whiteList.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}))
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
